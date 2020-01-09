575016b62a7bba91242975aff1b06da7f40b6ebe
sethusuresh <sethusuresh.ekm@gmail.com>
2020-01-10 00:02:51 +0530

    first commit

---
+++ src/main/java/com/iot/homeAutomation/HomeAutomationApplication.java
@@ -0,0 +1,14 @@
+package com.iot.homeAutomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeAutomationApplication.class, args);
+		System.out.println("Welcome to Home Automation Project");
+	}
+
+}
