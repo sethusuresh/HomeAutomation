ff5cc58778e20b0c68c241340a1bbfba71d4a84d
sethusuresh <sethusuresh.ekm@gmail.com>
2020-03-25 21:08:21 +0530

    adding keepalive

--- src/main/java/com/iot/homeAutomation/HomeAutomationApplication.java
+++ src/main/java/com/iot/homeAutomation/HomeAutomationApplication.java
@@ -2,13 +2,22 @@ package com.iot.homeAutomation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HomeAutomationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomeAutomationApplication.class, args);
 		System.out.println("Welcome to Home Automation Project");
+	}
+	
+	@RequestMapping(value = "/keepalive", method = RequestMethod.GET)
+	public String health() {
+		return "OK";
 	}
 
 }
