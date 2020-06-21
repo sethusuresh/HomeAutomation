86e338d8c64cd803905fbf0ecc9021340f20c4c4
sethusuresh <sethusuresh.ekm@gmail.com>
2020-06-21 20:32:25 +0530

    mongo configuration

--- src/main/java/com/iot/homeAutomation/Configurations/MongoConfiguration.java
+++ src/main/java/com/iot/homeAutomation/Configurations/MongoConfiguration.java
@@ -5,17 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 
 @Configuration
 public class MongoConfiguration {
-
+	
 	@Bean
-    public MongoClient mongo() {
-        return new MongoClient("localhost");
+    public MongoClient mongoClient() {
+        ServerAddress serverAddress = new ServerAddress("ssautohome.hopto.org", 27017);
+        MongoCredential credential = MongoCredential.createCredential("raspberrypi", "admin", "SS1994ekm@".toCharArray());
+        MongoClientOptions options = MongoClientOptions.builder().minConnectionsPerHost(5).build();
+        return new MongoClient(serverAddress, credential, options);
     }
  
     @Bean
     public MongoTemplate mongoTemplate() throws Exception {
-        return new MongoTemplate(mongo(), "Home_Automation");
+        return new MongoTemplate(mongoClient(), "Home_Automation");
     }
 }
