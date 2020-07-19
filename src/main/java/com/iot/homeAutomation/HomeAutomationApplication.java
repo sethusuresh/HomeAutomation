b65d022c2f92e733269bdb996c44559bf1baf5b3
sethusuresh <sethusuresh.ekm@gmail.com>
2020-07-19 12:18:00 +0530

    spring boot admin monitor support

--- src/main/java/com/iot/homeAutomation/HomeAutomationApplication.java
+++ src/main/java/com/iot/homeAutomation/HomeAutomationApplication.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +25,15 @@ public class HomeAutomationApplication {
 	public String health() {
 		logger.debug("keepalive");
 		return "OK";
+	}
+	
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+	    @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http.authorizeRequests().anyRequest().permitAll()  
+	            .and().csrf().disable();
+	    }
 	}
 
 }
