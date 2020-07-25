56a09198d9b0d38a5dfc8ac46d708e81b6a1ddfd
sethusuresh <sethusuresh.ekm@gmail.com>
2020-07-26 00:25:19 +0530

    SBA

--- src/main/java/com/iot/homeAutomation/HomeAutomationApplication.java
+++ src/main/java/com/iot/homeAutomation/HomeAutomationApplication.java
@@ -4,9 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,13 +24,4 @@ public class HomeAutomationApplication {
 		return "OK";
 	}
 	
-	@Configuration
-	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-	    @Override
-	    protected void configure(HttpSecurity http) throws Exception {
-	        http.authorizeRequests().anyRequest().permitAll()  
-	            .and().csrf().disable();
-	    }
-	}
-
 }
