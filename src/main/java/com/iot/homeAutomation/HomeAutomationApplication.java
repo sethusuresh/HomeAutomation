72a93eb3ef8bfb2ca46ddee37ddca5b997be67e6
sethusuresh <sethusuresh.ekm@gmail.com>
2020-07-26 01:00:13 +0530

    testing

--- src/main/java/com/iot/homeAutomation/HomeAutomationApplication.java
+++ src/main/java/com/iot/homeAutomation/HomeAutomationApplication.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +26,14 @@ public class HomeAutomationApplication {
 		logger.debug("keepalive is working");
 		return "OK";
 	}
+	
+	@Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()  
+                .and().csrf().disable();
+        }
+    }
 	
 }
